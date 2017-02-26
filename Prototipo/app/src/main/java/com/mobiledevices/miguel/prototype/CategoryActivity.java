73b73dac85167c4fb01c62fd665cc68727a38614
Miguel Angel <miguel.mzbi@gmail.com>
2017-02-26 13:11:30 -0600

    Prototype FInished MainActivity

--- Prototipo/app/src/main/java/com/mobiledevices/miguel/prototype/CategoryActivity.java
+++ Prototipo/app/src/main/java/com/mobiledevices/miguel/prototype/CategoryActivity.java
@@ -1,13 +1,21 @@
 package com.mobiledevices.miguel.prototype;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class CategoryActivity extends AppCompatActivity {
 
-    @Override
+    TextView equipmentName;
+
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_category);
+
+        Intent i = getIntent();
+        equipmentName = (TextView) findViewById(R.id.tview_title_category);
+        equipmentName.setText(i.getStringExtra("categoryName"));
     }
 }
